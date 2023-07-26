6a1f211e0649193af8a714baee0114484dbbff81
Adrian Molofsky <molofsky@stanford.edu>
2023-07-26 09:16:20 -0700

    Create 4-SeatsinTheater.py

---
+++ Arcade/The Core/4-SeatsinTheater.py
@@ -0,0 +1,11 @@
+"""
+Given the total number of rows and columns in the 
+theater (nRows and nCols, respectively), and the 
+row and column you're sitting in, return the 
+number of people who sit strictly behind you and in 
+your column or to the left, assuming all seats are 
+occupied.
+"""
+
+def solution(nCols, nRows, col, row):
+        return (nCols - col + 1) * (nRows - row)
